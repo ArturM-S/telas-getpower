79df70e031b5b0eedca3ccfa8437e49793465592
ArturM-S <silvamarquesartur@gmail.com>
2022-10-15 06:10:33 -0300

    chore:header complete

--- layout/Header/styles.ts
+++ layout/Header/styles.ts
@@ -3,4 +3,14 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
     height: 24px;
     width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 1rem;
+`;
+
+export const ProfileMenu = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
 `;
