9d0f413d6b54ec5662bba7a1213a12e82bbae4f4
ArturM-S <silvamarquesartur@gmail.com>
2022-10-15 06:55:20 -0300

    chore:Sidebar complete

--- layout/Sidebar/index.tsx
+++ layout/Sidebar/index.tsx
@@ -1,5 +1,71 @@
-import { Container } from './styles';
+import { MdHome } from 'react-icons/md';
+import { ImBooks } from 'react-icons/im';
+import { FaUniversity } from 'react-icons/fa';
+import { AiOutlineDownCircle, AiOutlineMinusCircle } from 'react-icons/ai';
+import { BsJournalBookmarkFill } from 'react-icons/bs';
+import {
+    Container,
+    DropDownIcon,
+    NavLogo,
+    NavLogoBox,
+    NavMenu,
+    NavMenuItem,
+    Picture,
+    ProfileEmail,
+    ProfileInfos,
+    ProfileName,
+} from './styles';
 
 export function Sidebar() {
-    return <Container>olá</Container>;
+    return (
+        <Container>
+            <NavLogoBox>
+                <NavLogo src="logo.png" alt="logo" />
+                academia do sol
+            </NavLogoBox>
+
+            <ProfileInfos>
+                <Picture />
+                <ProfileName>Thiago Abreu</ProfileName>
+                <ProfileEmail>folador@folador.com</ProfileEmail>
+            </ProfileInfos>
+            <NavMenu>
+                <NavMenuItem>
+                    <MdHome size={20} />
+                    Pagina inicial
+                </NavMenuItem>
+            </NavMenu>
+            <NavMenu>
+                Cursos
+                <NavMenuItem>
+                    <ImBooks size={20} />
+                    Meus cursos
+                </NavMenuItem>
+                <NavMenuItem>
+                    <FaUniversity size={20} />
+                    Todos os cursos
+                </NavMenuItem>
+            </NavMenu>
+            <NavMenu>
+                Dimensionamento
+                <NavMenuItem>
+                    <AiOutlineDownCircle size={20} />
+                    Baixa tensão
+                    <DropDownIcon>{'>'}</DropDownIcon>
+                </NavMenuItem>
+                <NavMenuItem>
+                    <AiOutlineMinusCircle size={20} />
+                    Média tensão
+                    <DropDownIcon>{'>'}</DropDownIcon>
+                </NavMenuItem>
+            </NavMenu>
+            <NavMenu>
+                crm
+                <NavMenuItem>
+                    <BsJournalBookmarkFill size={20} />
+                    Gerenciar
+                </NavMenuItem>
+            </NavMenu>
+        </Container>
+    );
 }
--- layout/Sidebar/styles.ts
+++ layout/Sidebar/styles.ts
@@ -1,7 +1,67 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
     height: 100vh;
     width: 350px;
     background-color: #01010d;
+    color: #f1f1f1;
+    padding: 1rem;
+`;
+
+export const NavLogoBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+`;
+
+export const NavLogo = styled.img`
+    height: 1rem;
+    width: 1rem;
+`;
+export const ProfileInfos = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+export const Picture = styled.img`
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+    border: 1px solid #f1f1f1;
+`;
+export const ProfileName = styled.h1`
+    font-size: 1rem;
+    font-weight: 400;
+`;
+export const ProfileEmail = styled.h2`
+    font-size: 0.7rem;
+    font-weight: 300;
+`;
+export const NavMenu = styled.div`
+    color: orange;
+`;
+export const NavMenuItem = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    padding: 0.5rem;
+    color: #f1f1f1;
+    :hover {
+        background-color: #3b3b3b4d;
+        filter: brightness(0.9);
+    }
+`;
+
+export const DropDownIcon = styled.h1`
+    font-size: 1rem;
+    font-weight: 400;
+    color: #f1f1f1;
+    display: flex;
+
+    align-self: flex-end;
 `;
